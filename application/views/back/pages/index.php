1d6aee7e8a36353d8fa3ebb9610a45ae2aa1bc8b
PiotrRybarczyk-php <piotr.rybarczyk1994@gmail.com>
2021-08-20 14:44:35 +0200

    small fixes

--- application/views/back/pages/index.php
+++ application/views/back/pages/index.php
@@ -1,10 +1,10 @@
     <!-- ########## START: MAIN PANEL ########## -->
     <div class="br-mainpanel">
       <div class="pd-30">
-        <h4 class="tx-gray-800 mg-b-5">Dashboard</h4>
+        <h4 class="tx-gray-800 mg-b-5">Pulpit</h4>
         <p class="mg-b-0"><?php echo subtitle(); ?></p>
       </div><!-- d-flex -->
 
       <div class="br-pagebody mg-t-5 pd-x-30">
 
-      	    <iframe src="<?php echo base_url(); ?>assets/instrukcja.pdf" width="100%" height="500px"></iframe>
+        <iframe src="<?= base_url('assets/') . 'instrukcja.pdf'; ?>" width="100%" height="500px"></iframe>
